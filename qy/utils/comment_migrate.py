00f1c2060c0112ad170857ce885e14a9ad5aaa38
zyi <54423910+cyfxiaofan@users.noreply.github.com>
2020-10-30 19:05:27 +0800

    Add files via upload
    update something

--- qy/utils/comment_migrate.py
+++ qy/utils/comment_migrate.py
@@ -4,10 +4,12 @@ import os
 import datetime
 from geoalchemy2 import Geometry, WKTElement
 from config import SELECT_sql, STATIC_FILE, INSERT_COLUMNS_sql, SELECT_KML_sql, SET_Maxid_sql, regex_list, \
-    SELECT_MAXID_sql
+    SELECT_MAXID_sql, UPDATE_sql, UPDATEALL_sql
 import pandas as pd
+import numpy as np
 from utils import pg_sqlhelper, sr_sqlhelper
 from utils.comment_exception import CustomError
+import copy
 
 
 class MigrateInterface(object):
@@ -50,6 +52,13 @@ class MigrateInterface(object):
         """
         return pd.read_sql(sql, con=self.pg.conn)
 
+    def get_data_pg(self):
+        """
+        获取 pg 的 Dataframe
+        :return: sql执行结果 dataframe
+        """
+        self.df = pd.read_sql(SELECT_sql.format(tablename=self.tablename), con=self.pg.conn)
+
     def save_infos(self, kw, data):
         """
         :param kw: 需要保存的类型
@@ -71,7 +80,59 @@ class MigrateInterface(object):
         :param field: 字段名
         :return: 指定的字段列表
         """
-        return self.df[field].values.tolist()
+        try:
+            return self.df[field].values.tolist()
+        except FutureWarning:
+            return self.df[field].values.tolist()
+
+    def get_field_replace(self, field, replace=None, default=None):
+        """
+        获取指定字段列表 并处理
+        :param replace: 默认不开启特殊替换
+        :param default: 默认替换为空
+        :param field: 字段名
+        :return: 指定的字段列表
+        """
+        if replace is None:
+            return self.df[field].replace(np.nan, '').values.tolist()
+        else:
+            return self.df[field].replace(np.nan, '').replace(replace, default, regex=True).values.tolist()
+
+    def format_time(self, _field, special=None):
+        """
+        格式化时间
+        :param special: 是否开启特殊匹配 指定spacial字典 格式： {'特殊字符':'替换的字符'}
+        :param _field: 需要格式化的时间字段
+        """
+        if special is not None:
+            _Time = self.get_field(_field)
+            for index, _ev in enumerate(_Time):
+                if _ev in special:
+                    _Time[index] = special[_ev]
+            self.add_columns(_field, _Time)
+        _Time = self.get_field_replace(_field, replace=['/', r'\.', '年', '月', '日', '号'],
+                                       default=['-', '-', '-', '-', '', ''])
+        new_A = []
+        for _i in _Time:
+            if not self.check_none(_i):
+                if _i[-1] == '-':
+                    new_A.append(_i[:-1])
+                else:
+                    new_A.append(_i)
+            else:
+                new_A.append(_i)
+        _new_T = []
+        for i in new_A:
+            if not self.check_none(i):
+                if len(i) < 5:
+                    _new_T.append(i + '-1-1')
+                elif 5 < len(i) < 8:
+                    _new_T.append(i + '-1')
+                else:
+                    _new_T.append(i)
+            else:
+                _new_T.append(i)
+        self.add_columns(_field, _new_T)
 
     def get_columns(self, index):
         """
@@ -118,8 +179,9 @@ class MigrateInterface(object):
         except AssertionError:
             return False
 
-    def map_fields(self, map_dict, oldfield, newfield):
+    def map_fields(self, map_dict, oldfield, newfield, drop=False):
         """
+        :param drop: 默认不删oldfield
         :param map_dict: 一个映射字典或是自定义函数
         :param oldfield: 需要映射匹配的字段
         :param newfield: 添加的新字段
@@ -127,18 +189,27 @@ class MigrateInterface(object):
         """
         _li = eval('self.df.{}.map'.format(oldfield))(map_dict)
         self.df[newfield] = _li
+        if drop:
+            self.drop_columns([oldfield])
 
-    def add_columns(self, field, data):
+    def add_columns(self, field, data, drop=None, flag=False):
         """
         添加一列
+        :param flag: 是否删除多条
+        :param drop: 是否指定删除某字段 默认不删
         :param field: 列名
         :param data: 列数据
-        :return: None
         """
         try:
             self.df[field] = data
         except Exception as e:
             raise CustomError('增加列错误：%s' % e)
+
+        if drop is not None:
+            try:
+                self.drop_columns([drop] if not flag else [drop])
+            except Exception as e:
+                raise CustomError('删除指定字段出错，错误信息：%s' % e)
 
     def get_title(self):
         """
@@ -157,7 +228,7 @@ class MigrateInterface(object):
         """
         :return: 将dataframe 去除 nan和 config的字符 之后返回 list
         """
-        return self.df.replace(pd.np.nan, None).replace(regex_list()[0], regex_list()[1], regex=True).values.tolist()
+        return self.df.replace(np.nan, None).replace(regex_list()[0], regex_list()[1], regex=True).values.tolist()
 
     def save_id_ralation(self):
         """
@@ -234,13 +305,41 @@ class MigrateInterface(object):
                 else:
                     raise CustomError('写入失败！失败原因：%s. 执行的sql语句为: %s' % (e, _sql))
 
+    def update_field(self, field, val, _id=None, many=False, index=None):
+        """
+        执行更新sql
+        :param index: 开启计数
+        :param many: 是否为批量更新
+        :param field: 更新的字段名
+        :param val: 更新的值
+        :param _id: 更新的的id
+        :return:
+        """
+        update = UPDATEALL_sql if many else UPDATE_sql
+        try:
+            self.pg.execute_modify_sql(
+                update.format(
+                    tablename=self.tablename,
+                    fieldname=field,
+                    val=val,
+                    id=_id
+                )
+            )
+            self.pg.commit()
+            print('更新成功') if not index else print('第%d条数据更新成功！' % index)
+            return True
+        except Exception as e:
+            self.pg.rollback()
+            raise CustomError('更新失败， 错误原因：%s' % e)
+
     def set_maxid_seq(self, tablename):
         """
-        将id索引设置成最大 此方法在数据迁完时候  ***必须要执行*****
+        将id索引设置成最大 此方法在数据迁完时候  *****必须要执行*****
         :param tablename: pg 的表名
         """
         self.pg.execute_modify_sql(SET_Maxid_sql.format(tablename=tablename))
         self.pg.commit()
+        print('%s 表 索引已更新' % tablename)
 
     def get_kml_data(self, fieldname):
         """
@@ -327,7 +426,7 @@ class MigrateInterface(object):
 
     def repare_field(self, field, length, default, special=False, replace=None):
         """
-        修理字段
+        repare字段
         :param replace: 特殊情况所替换的 str
         :param special: 默认不检测特殊情况
         :param length: check的field长度
@@ -368,6 +467,83 @@ class MigrateInterface(object):
             self.add_columns(default, [str(i).upper() for i in self.df[default]])
         except TypeError:
             raise CustomError('%s 转换 str 失败! 请为该方法指定 default' % default)
+
+    @classmethod
+    def check_none(cls, _str):
+        """
+        检验 _str 是否为空 或 None
+        :param _str:
+        :return: bool
+        """
+        if _str is None or _str.isspace() or _str == '':
+            return True
+        return False
+
+    def get_row_field(self, index, field, val=None, update=False):
+        """
+        获取指定列指定字段的值
+        :param val: 更新的指定值
+        :param update: 是否更新该字段
+        :param index: 索引
+        :param field: 字段
+        :return: 值
+        """
+        _index = self.get_title().index(field)
+        if not update:
+            try:
+                return self.get_columns(index)[_index]
+            except Exception as e:
+                raise CustomError('获取指定数据错误。错误信息：%s' % e)
+        else:
+            try:
+                self.df.loc[index, field] = val
+                return self.get_columns(index)[_index]
+            except Exception as e:
+                raise CustomError('更新指定位置数据错误。错误信息：%s' % e)
+
+    @classmethod
+    def check_type(cls, _str, _type):
+        """
+        检测字符是否符合类型
+        :param _str: 检测字符串
+        :param _type: 检测类型
+        :return: True or False
+        """
+        _type = eval(_type)
+        try:
+            _type(_str)
+            return True
+        except ValueError:
+            return False
+
+    def make_reationtouser(self, field, default='system_users'):
+        """
+        与 user表建立映射返回map_dict
+        :param default: 若未指定则默认用户表
+        :param field: 获取映射关系的字段
+        :return: map_dict
+        """
+        try:
+            sql_df = self.get_sqldf(SELECT_sql.format(
+                tablename=default
+            ))
+            assert sql_df is not None
+            new_userid_list = sql_df['id'].values.tolist()
+            new_field_list = sql_df[field].values.tolist()
+            assert new_field_list is not None
+            map_dict = {}
+            for _index in range(len(new_userid_list)):
+                map_dict[str(new_field_list[_index])] = new_userid_list[_index]
+            return map_dict
+        except AssertionError as e:
+            raise CustomError('与User表建立映射关系出错。错误：%s' % e)
+
+    def change_id(self):
+        self.add_columns('id', [i for i in range(1, self.count() + 1)], drop='Id')
+
+    @classmethod
+    def copy(cls, _list):
+        return copy.copy(_list)
 
     def show_kml(self):
         return self.kml_df.values.tolist()
